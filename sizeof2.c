5eff673ad1f404a9f14873432c1419a6cd37567b
danieljmanzano <djorgemanzano@gmail.com>
2024-05-13 20:33:54 -0300

    apaguei denovo

--- sizeof2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    long int n;    
-    scanf("%lu", &n);
-    printf("char: %lu\n", n * sizeof(char));
-    printf("int: %lu\n", n * sizeof(int));
-    printf("double: %lu\n", n * sizeof(double));
-}
-    
